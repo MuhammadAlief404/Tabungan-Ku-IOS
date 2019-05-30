219d8879000ea7367146dd3bbc1842f37fb50fe7
Muhammad Alief <alief.pribadi@gmail.com>
2019-05-30 07:24:34 -0700

    init project

---
+++ Tabungan Ku/OutcomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OutcomeViewController.swift
+//  Tabungan Ku
+//
+//  Created by Muhammad Alief on 28/05/19.
+//  Copyright © 2019 Muhammad Alief. All rights reserved.
+//
+
+import UIKit
+
+class OutcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
